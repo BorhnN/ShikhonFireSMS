fd5ba45779d4c13bce8433a86d87ab1fedc167b6
Borhan <borhan.ur@gmail.com>
2017-04-30 05:50:29 +0600

    avoided NullPointerException

--- app/src/main/java/org/shikhon/fireSMSserver/RegObject.java
+++ app/src/main/java/org/shikhon/fireSMSserver/RegObject.java
@@ -18,17 +18,28 @@ public class RegObject {
     String institute;
     String phoneNum;
     String couponCode;
+    String fullMessage;
 
     public RegObject() {
     }
 
-    public RegObject(String userName, String status, String institute, String phoneNum, String couponCode) {
-        this();
+    public RegObject(String none) {
+        userName = "";
+        status = "";
+        institute = "";
+        phoneNum = "";
+        couponCode = "";
+        fullMessage = "";
+    }
+
+    public RegObject(String userName, String status, String institute, String phoneNum, String couponCode, String fullMessage) {
+        this("");
         this.userName = userName;
         this.status = status;
         this.institute = institute;
         this.phoneNum = phoneNum;
         this.couponCode = couponCode;
+        this.fullMessage = fullMessage;
     }
 
     public String getUserName() {
@@ -70,20 +81,34 @@ public class RegObject {
     public void setCouponCode(String couponCode) {
         this.couponCode = couponCode;
     }
+
+    public String getFullMessage() {
+        return fullMessage;
+    }
+
+    public void setFullMessage(String fullMessage) {
+        this.fullMessage = fullMessage;
+    }
+
     public static RegObject fromRegSMS(String s){
-        String userName;
-        String status;
-        String institute;
-        String phoneNum;
-        String couponCode;
+        String userName = "";
+        String status = "";
+        String institute = "";
+        String phoneNum = "";
+        String couponCode = "";
         List<String> subStringList = Arrays.asList(s.split(","));
-        userName = subStringList.get(0).substring(4).trim();
-        status = subStringList.get(1).trim();
-        institute = subStringList.get(2).trim();
-        phoneNum = subStringList.get(3).trim();
-        couponCode = subStringList.get(4).trim();
+        try {
+            userName = subStringList.get(0).substring(4).trim();
+            status = subStringList.get(1).trim();
+            institute = subStringList.get(2).trim();
+            phoneNum = subStringList.get(3).trim();
+            couponCode = subStringList.get(4).trim();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         if (!Pattern.matches(PHONE_NUM_PATTERN, phoneNum)){
         }
-        return new RegObject(userName, status, institute, phoneNum, couponCode);
+        return new RegObject(userName, status, institute, phoneNum, couponCode, s);
     }
 }
