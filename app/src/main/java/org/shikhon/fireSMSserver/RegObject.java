8bcab0b98ebe927426bd6daff1e83efc2fa4771c
Borhan <borhan.ur@gmail.com>
2017-04-30 09:26:48 +0600

    almost final

--- app/src/main/java/org/shikhon/fireSMSserver/RegObject.java
+++ app/src/main/java/org/shikhon/fireSMSserver/RegObject.java
@@ -2,8 +2,12 @@ package org.shikhon.fireSMSserver;
 
 import android.util.Log;
 
+import com.google.firebase.database.ServerValue;
+
+import java.security.PrivilegedAction;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,6 +18,8 @@ import java.util.regex.Pattern;
 public class RegObject {
     public static final String PHONE_NUM_PATTERN = "01[56789]\\d{8}|\\+8801[56789]\\d{8}|008801[56789]\\d{8}";
     public static final String COUPON_PATTERN = "((c|C)\\-?)?(([0]*[0-9]{1,3})|1000)(?![\\w\\d])";
+    public static final String STATUS_SEARCH_PATTERN = "[\\s\\,]{1}(S|s|HI|hi|Hi|hI|HD|hd|Hd|hD|O|o|g|G|T|t)[\\s\\,]{1}";
+    public static final String STATUS_PATTERN = "(S|s|HI|hi|Hi|hI|HD|hd|Hd|hD|O|o|g|G|T|t)";
     private static final String TAG = "RegObject";
 
 
@@ -23,6 +29,7 @@ public class RegObject {
     String phoneNum;
     String couponCode;
     String fullMessage;
+    Map<String, String> timeStamp;
 
     public RegObject() {
     }
@@ -44,11 +51,14 @@ public class RegObject {
         this.phoneNum = phoneNum;
         this.couponCode = couponCode;
         this.fullMessage = fullMessage;
+        timeStamp = ServerValue.TIMESTAMP;
+
     }
 
     public String getUserName() {
         return userName;
     }
+
 
     public void setUserName(String userName) {
         this.userName = userName;
@@ -94,6 +104,14 @@ public class RegObject {
         this.fullMessage = fullMessage;
     }
 
+    public Map<String, String> getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Map<String, String> timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
     public static RegObject fromRegSMS(String s){
         String userName = "";
         String status = "";
@@ -110,7 +128,7 @@ public class RegObject {
             institute = subStringList.get(2).trim();
             phoneNum = subStringList.get(3).trim();
             phoneNumStarts = s.indexOf(phoneNum);
-            phoneNumEnds = s.lastIndexOf(phoneNum);
+            phoneNumEnds = phoneNumStarts+phoneNum.length();
             couponCode = subStringList.get(4).trim();
         } catch (Exception e) {
             e.printStackTrace();
@@ -123,16 +141,35 @@ public class RegObject {
             Matcher m = phonepattern.matcher(s);
             if (m.find()){
                 phoneNum = m.group(0);
+                phoneNumStarts = s.indexOf(phoneNum);
+                phoneNumEnds = phoneNumStarts+phoneNum.length();
             }
         }
 
         if (!Pattern.matches(COUPON_PATTERN, couponCode)){
             Pattern couponPattern =  Pattern.compile(COUPON_PATTERN);
-            Matcher m = couponPattern.matcher(s);
-            while (m.find()){
-                couponCode = m.group();
+            Matcher mS = couponPattern.matcher(s.substring(0, phoneNumStarts));
+            while (mS.find()){
+                couponCode = mS.group();
+            }
+            Matcher mE = couponPattern.matcher(s.substring(phoneNumEnds));
+            while (mE.find()){
+                couponCode = mE.group();
             }
         }
+        if (!Pattern.matches(STATUS_PATTERN, status)){
+            Pattern statusSearch = Pattern.compile(STATUS_SEARCH_PATTERN);
+            Matcher m = statusSearch.matcher(s);
+            while (m.find()){
+                String st = m.group();
+                Matcher sm = Pattern.compile(STATUS_PATTERN).matcher(st);
+                while (sm.find()){
+                    status = sm.group();
+                }
+            }
+        } else Log.d(TAG, "fromRegSMS: pattern did match");
+
+        Log.d(TAG, "fromRegSMS: "+couponCode);
 
         return new RegObject(userName, status, institute, phoneNum, couponCode, s);
     }
