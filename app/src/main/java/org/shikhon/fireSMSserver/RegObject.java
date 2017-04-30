48e1d69bd1911880061c6891afc070997ee9cfd0
Borhan <borhan.ur@gmail.com>
2017-04-30 08:05:28 +0600

    almost final

--- app/src/main/java/org/shikhon/fireSMSserver/RegObject.java
+++ app/src/main/java/org/shikhon/fireSMSserver/RegObject.java
@@ -1,7 +1,10 @@
 package org.shikhon.fireSMSserver;
 
+import android.util.Log;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -10,7 +13,8 @@ import java.util.regex.Pattern;
 
 public class RegObject {
     public static final String PHONE_NUM_PATTERN = "01[56789]\\d{8}|\\+8801[56789]\\d{8}|008801[56789]\\d{8}";
-
+    public static final String COUPON_PATTERN = "((c|C)\\-?)?(([0]*[0-9]{1,3})|1000)(?![\\w\\d])";
+    private static final String TAG = "RegObject";
 
 
     String userName;
@@ -96,19 +100,40 @@ public class RegObject {
         String institute = "";
         String phoneNum = "";
         String couponCode = "";
+        int phoneNumStarts = 0;
+        int phoneNumEnds = 0;
+
         List<String> subStringList = Arrays.asList(s.split(","));
         try {
             userName = subStringList.get(0).substring(4).trim();
             status = subStringList.get(1).trim();
             institute = subStringList.get(2).trim();
             phoneNum = subStringList.get(3).trim();
+            phoneNumStarts = s.indexOf(phoneNum);
+            phoneNumEnds = s.lastIndexOf(phoneNum);
             couponCode = subStringList.get(4).trim();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        Log.d(TAG, "fromRegSMS: "+phoneNumStarts + " " + phoneNumEnds);
+        // Lets see if couponCode is really a couponCode
         if (!Pattern.matches(PHONE_NUM_PATTERN, phoneNum)){
+            Pattern phonepattern = Pattern.compile(PHONE_NUM_PATTERN);
+            Matcher m = phonepattern.matcher(s);
+            if (m.find()){
+                phoneNum = m.group(0);
+            }
         }
+
+        if (!Pattern.matches(COUPON_PATTERN, couponCode)){
+            Pattern couponPattern =  Pattern.compile(COUPON_PATTERN);
+            Matcher m = couponPattern.matcher(s);
+            while (m.find()){
+                couponCode = m.group();
+            }
+        }
+
         return new RegObject(userName, status, institute, phoneNum, couponCode, s);
     }
 }
